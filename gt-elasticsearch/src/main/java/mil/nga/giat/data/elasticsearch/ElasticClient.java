13840f539e510d31f5607eac4aa29866c8d6fb67
Brandon Miller <johndeereguy@gmail.com>
2018-12-04 21:52:48 -0500

    Completing format updates, replacing tabs with 4 spaces.

--- gt-elasticsearch/src/main/java/mil/nga/giat/data/elasticsearch/ElasticClient.java
+++ gt-elasticsearch/src/main/java/mil/nga/giat/data/elasticsearch/ElasticClient.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public interface ElasticClient extends Closeable {
 
     static final String RUN_AS = "es-security-runas-user";
-	
+
     public double getVersion();
 
     public List<String> getTypes(String indexName) throws IOException;
